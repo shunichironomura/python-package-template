5cf9560cc04c077f2926b71cbbb81961cde32af2
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2025-06-08 15:14:57 +0900

    Set-up CI

---
+++ tests/test_square.py
@@ -0,0 +1,15 @@
+import pytest
+
+
+@pytest.mark.parametrize(
+    ("x", "expected"),
+    [
+        (2, 4),
+        (3, 9),
+        (4, 16),
+        (5, 25),
+    ],
+)
+def test_square(x: int, expected: int) -> None:
+    from package_name_goes_here import square
+    assert square(x) == expected
